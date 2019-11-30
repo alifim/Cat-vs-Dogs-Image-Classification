5518ea333942c3483fcd7798bf583918bfd88109
Alif Ilham Madani <31367589+alifim@users.noreply.github.com>
2019-11-30 16:01:40 +0700

    Add files via upload

---
+++ testing_image_classifier.py
@@ -0,0 +1,55 @@
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import load_model
+from keras.models import model_from_json
+import pandas as pd
+import json
+import cv2
+import numpy as np
+
+test_data_dir = 'data/test'
+
+with open('model_in_json.json','r') as f:
+    model_json = json.load(f)
+
+model = model_from_json(model_json)
+model.load_weights('first_try.h5')
+
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+test_generator = test_datagen.flow_from_directory(
+    test_data_dir,
+    target_size=(150, 150),
+    color_mode="rgb",
+    batch_size=1,
+    class_mode=None,
+    shuffle=False,
+    seed=42
+)
+
+STEP_SIZE_TEST=test_generator.n//test_generator.batch_size
+test_generator.reset()
+pred=model.predict_generator(test_generator,
+    steps=STEP_SIZE_TEST,
+    verbose=1)
+
+predicted_class_indices=np.where(pred>0.5,'dogs','cats')
+
+predictions = predicted_class_indices.tolist()
+
+filenames=test_generator.filenames
+results=pd.DataFrame({"Filename":filenames,
+                      "Predictions":predictions})
+results.to_csv("results.csv",index=False)
+'''
+img = cv2.imread('data/test/5.jpg')
+img = cv2.resize(img,(150,150))
+img = np.reshape(img,[1,150,150,3])
+
+classes = model.predict_classes(img)
+
+print(classes)
+'''
