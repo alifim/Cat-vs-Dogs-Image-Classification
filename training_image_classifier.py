00bf70a268adbcf389c11ded4d40870a0dbcbd88
Alif Ilham Madani <31367589+alifim@users.noreply.github.com>
2019-12-07 12:50:07 +0700

    Add files via upload

--- training_image_classifier.py
+++ training_image_classifier.py
@@ -1,41 +1,3 @@
-'''This script goes along the blog post
-"Building powerful image classification models using very little data"
-from blog.keras.io.
-It uses data that can be downloaded at:
-https://www.kaggle.com/c/dogs-vs-cats/data
-In our setup, we:
-- created a data/ folder
-- created train/ and validation/ subfolders inside data/
-- created cats/ and dogs/ subfolders inside train/ and validation/
-- put the cat pictures index 0-999 in data/train/cats
-- put the cat pictures index 1000-1400 in data/validation/cats
-- put the dogs pictures index 12500-13499 in data/train/dogs
-- put the dog pictures index 13500-13900 in data/validation/dogs
-So that we have 1000 training examples for each class, and 400 validation examples for each class.
-In summary, this is our directory structure:
-```
-data/
-    train/
-        dogs/
-            dog001.jpg
-            dog002.jpg
-            ...
-        cats/
-            cat001.jpg
-            cat002.jpg
-            ...
-    validation/
-        dogs/
-            dog001.jpg
-            dog002.jpg
-            ...
-        cats/
-            cat001.jpg
-            cat002.jpg
-            ...
-```
-'''
-
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
@@ -48,9 +10,9 @@ img_width, img_height = 150, 150
 
 train_data_dir = 'data/train'
 validation_data_dir = 'data/validation'
-nb_train_samples = 2000
-nb_validation_samples = 800
-epochs = 10
+nb_train_samples = 13223
+nb_validation_samples = 4325
+epochs = 15
 batch_size = 16
 
 if K.image_data_format() == 'channels_first':
@@ -74,11 +36,11 @@ model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
 model.add(Dense(64))
 model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
+model.add(Dropout(0.25))
+model.add(Dense(7))
+model.add(Activation('softmax'))
 
-model.compile(loss='binary_crossentropy',
+model.compile(loss='categorical_crossentropy',
               optimizer='rmsprop',
               metrics=['accuracy'])
 
@@ -96,15 +58,17 @@ validation_datagen = ImageDataGenerator(rescale=1. / 255)
 train_generator = train_datagen.flow_from_directory(
     train_data_dir,
     target_size=(img_width, img_height),
+    color_mode="rgb",
     batch_size=batch_size,
-    class_mode='binary')
+    class_mode='categorical')
 
 
 validation_generator = validation_datagen.flow_from_directory(
     validation_data_dir,
     target_size=(img_width, img_height),
+    color_mode="rgb",
     batch_size=batch_size,
-    class_mode='binary')
+    class_mode='categorical')
 
 model.fit_generator(
     train_generator,
@@ -114,7 +78,7 @@ model.fit_generator(
     validation_steps=nb_validation_samples // batch_size)
 
 model_json = model.to_json()
-with open("model_in_json.json", "w") as json_file:
+with open("various_vehicles_model.json", "w") as json_file:
     json.dump(model_json, json_file)
 
-model.save_weights('first_try.h5')
+model.save_weights('various_vehicles_weight.h5')
